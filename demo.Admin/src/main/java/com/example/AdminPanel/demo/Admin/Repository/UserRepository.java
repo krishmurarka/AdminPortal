84be05f356b167aa9a7d7de1637cc700dd118ab7
Taranpreet Singh Chabbra <singhh3010@gmail.com>
2021-12-07 23:21:00 +0530

    updated_userrepo

--- demo.Admin/src/main/java/com/example/AdminPanel/demo/Admin/Repository/UserRepository.java
+++ demo.Admin/src/main/java/com/example/AdminPanel/demo/Admin/Repository/UserRepository.java
@@ -2,8 +2,21 @@ package com.example.AdminPanel.demo.Admin.Repository;
 
 import com.example.AdminPanel.demo.Admin.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     public User findByUsername(String username);
+
+    public User findByEmail(String email);
+
+    public User findByResetPasswordToken(String resetPasswordToken);
+
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE users t SET t.reset_password_token =:token where t.email= :email",nativeQuery = true)
+    public void updateUser(@Param(value = "email") String email, @Param(value = "token") String token);
 
 }

22e6610f10cc8b3be34705a1e7e102d9d7913003
Taranpreet Singh Chabbra <singhh3010@gmail.com>
2021-12-07 23:21:31 +0530

    updated

--- demo.Admin/src/main/java/com/example/AdminPanel/demo/Admin/Service/UserService.java
+++ demo.Admin/src/main/java/com/example/AdminPanel/demo/Admin/Service/UserService.java
@@ -12,4 +12,11 @@ public interface UserService {
     public void deleteUser(long userId);
 
     public void updateUser(User user);
+
+
+    void updateResetPasswordToken(String token, String email);
+
+    User get(String reset_password_token);
+
+    void updatePassword(User user, String newPassword);
 }
